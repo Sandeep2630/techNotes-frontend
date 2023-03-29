8a3743f551087bc0ddbc9cdcc2e5a82516a7e46d
sandeep2630 <katwal.sandeep28@gmail.com>
2023-03-29 10:50:20 +0530

    added all outlet for protected and private routes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,6 @@
-import React from 'react'
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 export default function Layout() {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <Outlet />;
 }
