6f428ea5341ba2a652183629c2004207fda1e8d6
sandeep2630 <katwal.sandeep28@gmail.com>
2023-03-31 10:45:49 +0530

    rtk query

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { apiSlice } from "./api/apiSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
 });
